c9bae1558d7ce0e55fcd664e39c08dcaaf62b28c
bayram.bayranbekov <bayram.bayranbekov@citeck.ru>
2025-04-29 20:18:19 +0300

    love pizza

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -43,8 +43,7 @@ const Main: React.FC = () => {
 
     dispatch(asyncLoadPizza(search));
   };
-  console.log('Вкусная пицца');
-  console.log('Вкусная пицца 2222');
+  console.log('love pizza');
 
   React.useEffect(() => {
     if (window.location.search) {
